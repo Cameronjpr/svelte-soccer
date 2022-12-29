ef95c9921110e05860c055f6f92fd6699911f9ef
cameronjpr <cameronjpr@gmail.com>
2022-12-29 15:12:32 +0000

    refine login page

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,25 +1,14 @@
 <script>
 	import { page } from '$app/stores';
-
-	async function signInWithEmail() {
-		const { data, error } = await supabase.auth.signInWithOtp({
-			email: 'example@email.com',
-			options: {
-				emailRedirectTo: 'https://example.com/welcome'
-			}
-		});
-	}
 </script>
 
 {#if !$page.data.session}
-	<h1>I am not logged in</h1>
+	<h1>Enter your email to get started</h1>
+	<form method="POST">
+		<input type="text" name="email" placeholder="email" />
+		<label for="email">Email</label>
+		<button>Sign in</button>
+	</form>
 {:else}
 	<h1>Welcome {$page.data.session.user.email}</h1>
-	<p>I am logged in!</p>
 {/if}
-
-<form method="POST">
-	<input type="text" name="email" placeholder="email" />
-	<label for="email">Email</label>
-	<button>Log in</button>
-</form>
