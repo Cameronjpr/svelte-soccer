8cd59c7d5bea88faf4f491da6dbd0d5c21adffc1
cameronjpr <cameronjpr@gmail.com>
2023-02-24 21:02:21 +0000

    fix 500 error

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,18 @@
 import type { LayoutServerLoad } from './$types';
 
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
-import { fail, redirect } from '@sveltejs/kit';
-import { getActiveGameweek } from '@lib/util/gameweek';
 
 export const load: LayoutServerLoad = async (event) => {
 	const { fetch } = event;
 	const res = await fetch('/api/active-gameweek');
-	const activeGameweek = await res.json();
+	let activeGameweek = 1;
+
+	if (res.ok) {
+		activeGameweek = await res.json();
+	}
 
 	return {
 		session: await getServerSession(event),
-		activeGameweek: activeGameweek ?? 1
+		activeGameweek: activeGameweek || 1
 	};
 };
