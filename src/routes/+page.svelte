c6e1162a714191502ccbbbd6f0fbf338c3b431bd
cameronjpr <cameronjpr@gmail.com>
2023-02-28 22:50:08 +0000

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <h1>Welcome to PremPredictor</h1>
-
-<h2>How to play</h2>
-
-<p>
-	Sign up for a free account, then pick a team for each gameweek. You can change your team as many
-	times as you like before the deadline.
-</p>
+<strong>22/23 season</strong>
