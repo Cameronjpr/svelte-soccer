dd2e0603e91fdbb79feb759efebdfd88f2faf7c3
cameronjpr <cameronjpr@gmail.com>
2023-08-09 14:31:46 +0100

    stupid method of scoring

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -18,33 +18,46 @@ export const actions: Actions = {
 
     if (res.ok) {
       // Find selections for this user
+      const { data: users } = await supabaseClient.from('Users').select();
       const { data: selections } = await supabaseClient.from('Selections').select();
       const fixtures: Fixture[] = await res.json();
 
+      users?.forEach(async (u) => {
+        console.log(u)
+        let tally = 0;
+        selections?.forEach((s) => {
+          console.log(s)
+          const fixture = fixtures.find(f => f.code === s.fixture)
+          console.log(fixture)
+          const { team_a_score, team_h_score, team_a, team_h, finished_provisional } = fixture as Fixture;
 
-      selections?.forEach((s) => {
-        console.log(s)
-        const fixture = fixtures.find(f => f.code === s.fixture)
-        console.log(fixture)
-        const { team_a_score, team_h_score, team_a, team_h } = fixture
-
-        if (team_h_score === team_a_score) {
-          return;
-        }
-
-        if (team_h_score > team_a_score) {
-          if (s.selection === team_h) {
-            console.log('winning selection')
-          } else {
-            console.log('losing selection')
+          if (!finished_provisional || team_a_score === null || team_h_score === null) {
+            return;
           }
-        } else {
-          if (s.selection === team_a) {
-            console.log('wining selection')
-          } else {
-            console.log('losing selection')
+
+          if (team_h_score === team_a_score) {
+            return;
           }
-        }
+
+
+          if (team_h_score > team_a_score) { // home win
+            if (s.selection === team_h) {
+              tally++
+            } else {
+              tally--
+            }
+          } else { // away win
+            if (s.selection === team_a) {
+              tally++
+            } else {
+              tally--
+            }
+          }
+        });
+
+        const { error, status, data } = await supabaseClient.from('Users').update({ score: tally }).eq('id', u.id);
+
+        console.log({ error, status, data })
       });
 
       return {
