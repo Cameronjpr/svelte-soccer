131369b96f6e33c2e7688dff4724706478209c1d
cameronjpr <cameronjpr@gmail.com>
2023-08-10 19:26:55 +0100

    analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,10 +3,14 @@ import { invalidate } from '$app/navigation'
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 import type { LayoutLoad } from './$types'
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
 import type { Database } from '../database.types'
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
+
+  inject({ mode: dev ? 'development' : 'production' });
 
   const res = await fetch('/api/active-gameweek');
   let activeGameweek = 1;
