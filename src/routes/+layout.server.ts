9171afe51fafca1f5dfc29ad5b3f4409c968721b
cameronjpr <cameronjpr@gmail.com>
2023-08-12 18:44:02 +0100

    tidy

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,31 +1,8 @@
-export const load = async ({ fetch, locals: { getSession, supabase } }) => {
-	const res = await fetch('/api/active-gameweek');
-	let activeGameweek = 1;
-
-	if (res.ok) {
-		activeGameweek = await res.json();
-	}
-
+export const load = async ({ locals: { getSession } }) => {
 	const session = await getSession();
 
 	return {
 		session,
-		streamed: {
-			fixtures: await getFixtures(fetch)
-		},
-		activeGameweek: activeGameweek || 1
 	};
+
 };
-
-
-async function getFixtures(
-	fetch: (input: URL | RequestInfo, init?: RequestInit | undefined) => Promise<Response>,
-) {
-	const res = await fetch(`/api/fixtures`);
-
-	if (!res.ok) {
-		return null
-	}
-
-	return await res.json();
-}
