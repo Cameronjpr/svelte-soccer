69bed1ef69b4cf0e87ca1e44c7ca6077bec90bcd
cameronjpr <cameronjpr@gmail.com>
2023-08-13 20:38:08 +0100

    paginator

--- src/lib/Paginator/SimplePaginator.svelte
+++ src/lib/Paginator/SimplePaginator.svelte
@@ -13,7 +13,7 @@
 	class="text-gray-800 dark:text-gray-800 py-1 rounded-lg grid grid-cols-[1fr_1fr_1fr] items-center"
 >
 	<button
-		class={`flex items-center gap-1  justify-self-start ${
+		class={`flex items-center gap-1 dark:text-white justify-self-start ${
 			currentGameweek === 1 ? 'opacity-0' : ''
 		}`}
 		on:click={() => setGameweek(currentGameweek === 1 ? 1 : prevGameweek)}
@@ -25,7 +25,9 @@
 		Week {currentGameweek}
 	</h2>
 	<button
-		class={`flex items-center gap-1 justify-self-end ${currentGameweek === 38 ? 'opacity-0' : ''}`}
+		class={`flex items-center gap-1 dark:text-white  justify-self-end ${
+			currentGameweek === 38 ? 'opacity-0' : ''
+		}`}
 		on:click={() => setGameweek(currentGameweek === 38 ? 38 : nextGameweek)}
 		data-sveltekit-noscroll>Next<ChevRight /></button
 	>
