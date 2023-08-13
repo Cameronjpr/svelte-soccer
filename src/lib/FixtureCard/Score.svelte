d3eb3adc3ced133333d5cc4c4a708f4477c87d4a
cameronjpr <cameronjpr@gmail.com>
2023-08-13 14:18:14 +0100

    new stuff

---
+++ src/lib/FixtureCard/Score.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Fixture } from '@lib/types';
+
+	export let fixture: Fixture;
+
+	const { team_h_score, team_a_score, started, kickoff_time } = fixture;
+</script>
+
+{#if started}
+	{#if team_h_score != null && team_a_score != null}
+		<span
+			class="text-sm {fixture?.finished_provisional
+				? ''
+				: 'bg-red-500 rounded-lg font-semibold text-white'}"
+			>{team_h_score} : {team_a_score}
+		</span>
+	{:else}
+		<span id="score" class="text-sm">Delayed</span>
+	{/if}
+{:else}
+	<span id="kickoff-time" class="text-sm"
+		>{new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(new Date(kickoff_time))}</span
+	>
+{/if}
---
+++ src/lib/FixtureCard/SelectButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Team } from '@lib/types';
+	import { fade } from 'svelte/transition';
+
+	export let team: Team;
+	export let isSelectable: boolean;
+	export let isHome = false;
+	export let selectionLoading: boolean;
+	export let gameweekSelection: number | undefined;
+	export let handlePreselect: (teamId: number) => void;
+</script>
+
+<button
+	disabled={!isSelectable || gameweekSelection == team.id}
+	class="
+    font-semibold border-0 rounded p-2 text-center h-12
+    {selectionLoading ? 'animate-pulse' : ''}
+		{gameweekSelection == team.id ? 'bg-green-300' : 'bg-gray-200'}"
+	on:click={() => handlePreselect(team.id)}
+	style="border-color: {team.primaryColor}; border-{isHome ? 'left' : 'right'}-width: 4px;"
+>
+	{team.shortName}
+</button>
