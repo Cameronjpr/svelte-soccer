10b33974513f2e6c0db000d818ca97d78497c50a
cameronjpr <cameronjpr@gmail.com>
2023-08-15 17:39:53 +0100

    show gw2

--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -9,14 +9,14 @@ export const load = (async ({ parent, locals: {
 	const { data: users } = await supabase.from('Users').select('username,score,auth_user');
 	const { data: selections } = await supabase.from('Selections').select()
 
-	const selectionsForGameweek = selections.filter(s => s.gameweek === 1)
+	const selectionsForUpcomingGameweek = selections.filter(s => s.gameweek === 2)
 
 
 	const safeUsers = users.map((user: User) => {
 		return {
 			username: user.username,
 			score: user.score,
-			selection: selectionsForGameweek.find(s => s.selector === user.auth_user),
+			selection: selectionsForUpcomingGameweek.find(s => s.selector === user.auth_user),
 			auth_user: user.auth_user
 		}
 	})
