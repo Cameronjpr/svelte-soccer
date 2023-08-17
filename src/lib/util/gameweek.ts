3fe2895f2cbb008b612bf90f9b0a4fcb7aaa98c8
cameronjpr <cameronjpr@gmail.com>
2023-08-17 19:44:03 +0100

    handle errors

--- src/lib/util/gameweek.ts
+++ src/lib/util/gameweek.ts
@@ -4,7 +4,7 @@ import dayjs from 'dayjs';
 export const getActiveGameweek = (fixtures: Array<Fixture>): number => {
 	const now = dayjs();
 
-	const activeGameweek = fixtures.find((fixture) => {
+	const activeGameweek = fixtures?.find((fixture) => {
 		const kickoffTime = dayjs(fixture.kickoff_time);
 		return kickoffTime.isBefore(now);
 	});
@@ -15,7 +15,7 @@ export const getActiveGameweek = (fixtures: Array<Fixture>): number => {
 export const getUpcomingGameweek = (fixtures: Array<Fixture>): number => {
 	const now = dayjs();
 
-	const upcomingGameweek = fixtures.find((fixture) => {
+	const upcomingGameweek = fixtures?.find((fixture) => {
 		const kickoffTime = dayjs(fixture.kickoff_time);
 		return kickoffTime.isAfter(now);
 	});
