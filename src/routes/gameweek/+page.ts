a67207a14a5625b34ed5214fcb8c713a2cf0cec3
cameronjpr <cameronjpr@gmail.com>
2023-08-19 15:57:46 +0100

    tidy

--- src/routes/gameweek/+page.ts
+++ src/routes/gameweek/+page.ts
@@ -1,7 +1,3 @@
-import { getActiveGameweek } from '@lib/util/gameweek';
-import type { PageLoad } from './$types';
-import type { Selection } from '@lib/types';
-
 export async function load({ parent }) {
 	const { streamed: { fixtures, selections }, currentGameweek } = await parent();
 
